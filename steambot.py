4e5504aba62269c93936ebc04162fc24f9e601ed
Vulp1s06 <109761706+Vulp1s06@users.noreply.github.com>
2022-08-16 19:46:51 +0300

    Update steambot.py

--- steambot.py
+++ steambot.py
@@ -5,19 +5,24 @@ from bs4 import BeautifulSoup
 
 
 def search():
-    url = "https://steamcommunity.com/id/vulp1ss/games/?tab=all"
-    values = 'abcdefghijklmnopqrstuvwxyz">_/'
+    # in steam profile, go to games than copy the link
+    url = "https://steamcommunity.com/id/pintipanda/games/?tab=all"
+
+    # add the exceptions (something from the game's store page)
     exceptions = [
         "Rocket League",
         "Assassin's Creed III",
         "no longer available for sale on Steam.",
     ]
+
     data = requests.get(url).text
     data = re.search(r"var rgGames = (.*]);", data).group(1)
     data = json.loads(data)
     prices = []
     real_prices = []
     counter = 0
+    values = 'abcdefghijklmnopqrstuvwxyz">_/'
+    
     for d in data:
         print(d["name"])
         game_url = requests.get(
@@ -25,11 +30,15 @@ def search():
         ).text
         soup = BeautifulSoup(game_url, "lxml")
         price_div = str(soup.find("div", {"class": "game_purchase_action"}))
+        
+        # check if the game is free
         if "Free" in price_div:
             print("This is a free game")
             continue
+        # check for the exceptions
         if any(exception in str(soup) for exception in exceptions):
             continue
+        # get the game's current discounted value if exists
         if str(soup.find("div", {"class": "discount_final_price"})) in str(price_div):
             try:
                 discount_div = str(soup.find("div", {"class": "discount_final_price"}))
@@ -45,9 +54,10 @@ def search():
                 counter += 1
                 continue
             except ValueError:
-                print("There is a problem.")
+                print("There is a problem. (Invalid Game)")
                 counter += 1
                 continue
+        # get the game's current value if exists
         else:
             try:
                 game_price_div = str(
@@ -65,10 +75,11 @@ def search():
                 counter += 1
                 continue
             except ValueError:
-                print("!!!!!!There is a problem.")
+                print("There is a problem. (Invalid Game)")
                 counter += 1
                 continue
-
+    
+    # calculate the total of real_prices
     total = 0
     for i in real_prices:
         try:
